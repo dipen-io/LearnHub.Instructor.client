42e125b198d151636f49b095fd1b1ebb6f5cace8
Dipen Boro <borod9200@gmail.com>
2026-09-05 19:10:43 +0530

    fixing courseService api

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,50 +1,51 @@
 import { axiosInstance } from "@/lib/axios";
 
 interface CourseFormData {
-  courseTitle: string;
-  price: string;
-  isFree: string;
-  level: string;
-  language: string;
-  duration: string;
-  discount: string;
-  category: string;
-  tags: string;
-  requirements: string;
-  description: string;
-  whatYouWillLearn: string;
-  courseThumbnail: File | null;
-  promoVideoUrl: File | null;
+    courseTitle: string;
+    price: string;
+    isFree: string;
+    level: string;
+    language: string;
+    duration: string;
+    discount: string;
+    category: string;
+    tags: string;
+    requirements: string;
+    description: string;
+    whatYouWillLearn: string;
+    courseThumbnail: File | null;
+    promoVideoUrl: File | null;
 }
 
 // GET ALL COURSES :
-export const getCourses = async() => {
+export const getCourses = async () => {
     const { data } = await axiosInstance.get("/course");
     console.log("api/v1/course", data);
     return data;
 }
 
 // GET SINGLE SINGLE:
-export const getSingleCourseById = async(videoId: number) => {
+export const getSingleCourseById = async (videoId: number) => {
     const { data } = await axiosInstance.get(`/videos/${videoId}`);
     return data;
 }
 
 // Create Courses:
-export const createCourse =  async ( courseData: CourseFormData, instructorId: number ) => {
+export const createCourse = async (courseData: CourseFormData, instructorId: number) => {
     const formData = new FormData();
     Object.entries(courseData).forEach(([key, value]) => {
-         if (value !== null && value !== "") {
-               formData.append(key, value);
-         }
+        if (value !== null && value !== "") {
+            formData.append(key, value);
+        }
     });
 
     for (const [key, value] of formData.entries()) {
-       console.log("FormData entry:", key, value);
+        console.log("FormData entry:", key, value);
     }
     const url = `/course/new?instructorId=${encodeURIComponent(instructorId)}`;
     // const { data } = await axiosInstance.post(`/course/new` ,formData);
     const { data } = await axiosInstance.post(url, formData);
+    console.log("what is not found", data);
     return data;
 }
 
